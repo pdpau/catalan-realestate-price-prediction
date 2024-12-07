15167831aa3e8d2f7a90e00b1ccab4a39c2681bd
pdpau <56670577+pdpau@users.noreply.github.com>
2024-12-07 14:13:20 +0100

    working, falta millorar el model

--- web/app_pages/price_prediction.py
+++ web/app_pages/price_prediction.py
@@ -13,29 +13,29 @@ def load_data():
 
 @st.cache_resource
 def load_model():
-    with open('model_catalunya.pkl', 'rb') as f:
+    with open('pkl_files/model_catalunya.pkl', 'rb') as f:
         model = pickle.load(f)
     return model
 
 @st.cache_resource
 def load_label_encoders():
-    with open('le_location.pkl', 'rb') as f:
+    with open('pkl_files/le_location_catalunya.pkl', 'rb') as f:
         le_location = pickle.load(f)
-    with open('le_type.pkl', 'rb') as f:
+    with open('pkl_files/le_type_catalunya.pkl', 'rb') as f:
         le_type = pickle.load(f)
-    with open('le_region.pkl', 'rb') as f:
-        le_region = pickle.load(f)
-    return le_location, le_type, le_region
+    """ with open('pkl_files/le_region_catalunya.pkl', 'rb') as f:
+        le_region = pickle.load(f) """
+    return le_location, le_type
 # -- -- #
 
 
 def app():
     data = load_data()
-    data = data.drop(columns=['price/m2']) # Eliminem la columna 'price/m2' perquè no la utilitzem en la predicció
+    data = data.drop(columns=['price/m2', 'region']) # Eliminem les columnes 'price/m2' i 'region' perquè no les utilitzem en la predicció
 
     lista_locations = data['location'].unique()
     lista_types = data['type'].unique()
-    lista_regions = data['region'].unique()
+    #lista_regions = data['region'].unique()
 
     st.title('Predicción del precio de tu vivienda en Cataluña')
 
@@ -44,7 +44,7 @@ def app():
     with tab1:
         st.header('Predecir tu precio')
 
-        col1, col2, col3, col4, col5, col6 = st.columns(6) # TODO: Ens quedem amb "location" o amb "region"??
+        col1, col2, col3, col4, col5 = st.columns(5) # Ens quedem amb "location" i descartem "region"
         with col1:
             st.subheader('Title')
             location = st.selectbox(
@@ -71,32 +71,23 @@ def app():
                 lista_types
             )
 
-        with col6:
-            st.subheader('Title')
-            region = st.selectbox(
-                'Región',
-                lista_regions
-            )
-
         # -- Predicción -- #
-        #TODO: Botón + cargar modelo y label encoders + predecir
-        #model = load_model()
-        #le_location, le_type, le_region = load_label_encoders()
-        #location_encoded = le_location.transform([location])[0]
-        #type_encoded = le_type.transform([type])[0]
+        model = load_model()
+        le_location, le_type = load_label_encoders()
+        location_encoded = le_location.transform([location])[0]
+        type_encoded = le_type.transform([type])[0]
         #region_encoded = le_region.transform([region])[0]
 
-        """ pred_data = pd.DataFrame({
-            'location': [location_encoded],
-            'size': [size],
-            'rooms': [rooms],
-            'bathrooms': [bathrooms],
-            'type': [type_encoded],
-            'region': [region_encoded]
-        }) """
+        input_data = [location_encoded, size, rooms, bathrooms, type_encoded]
+        input_df = pd.DataFrame([input_data], columns=model.feature_names_in_)
+
+        prediction = model.predict(input_df)
+        prediction = int(round(prediction[0], 2))
 
         if st.button('Predecir precio'):
-            st.write('Precio predicho: xxx.xxx €')
+            st.write(f'Precio predicho: {prediction} €')
+            st.write('---')
+            st.write(f'Nombre de vivendes de referencia a {location}: {len(data[data["location"] == location])}')
         # -- -- #
 
     with tab2:
